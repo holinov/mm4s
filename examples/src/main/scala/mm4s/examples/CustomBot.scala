fb3d367ae3c2ef11d71e5b7285827109ff0e42b2
John Wass <jwass3@gmail.com>
2016-03-08 23:37:40 -0500

    adding custom bot to examples project, to be built as a Docker image

---
+++ examples/src/main/scala/mm4s/examples/CustomBot.scala
@@ -0,0 +1,26 @@
+package mm4s.examples
+
+import akka.actor._
+import mm4s.bots.api.{Message, BotID, Ready}
+import net.codingwell.scalaguice.ScalaModule
+
+
+class CustomBot extends Actor with mm4s.bots.api.Bot with ActorLogging {
+  var api: Option[ActorRef] = None
+  var id: Option[BotID] = None
+
+  def receive: Receive = {
+    case Ready(mm, botid) =>
+      api = Option(mm)
+      id = Option(botid)
+      log.debug("Bot [{}] ready", id.get /* hack */ .username)
+
+      api.foreach(_ ! Message("Here's Botty!"))
+  }
+}
+
+class CustomBotModule extends ScalaModule {
+  def configure(): Unit = {
+    bind[mm4s.bots.api.Bot].to[CustomBot]
+  }
+}

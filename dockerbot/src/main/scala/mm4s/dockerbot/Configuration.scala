951169da45809d9223c3d45a755b5292adb66bef
John Wass <jwass3@gmail.com>
2016-03-12 14:31:16 -0500

    roughed in some configuration capability that allows the resolved config to be injected #6

---
+++ dockerbot/src/main/scala/mm4s/dockerbot/Configuration.scala
@@ -0,0 +1,35 @@
+package mm4s.dockerbot
+
+import com.typesafe.config.{Config, ConfigFactory}
+import net.ceedubs.ficus.Ficus._
+
+import scala.collection.JavaConversions._
+
+/**
+ *
+ */
+object Configuration {
+
+  def build(config: Config = ConfigFactory.load())(implicit envs: Map[String, String] = sys.env): Config = {
+    import ConfigKeys._
+    import ConfigKeys.defaults._
+    val host = resolve(env.host, key.host, mmHost, config, envs)
+    val port = resolve(env.port, key.port, mmPort, config, envs)
+    val user = resolve(env.user, key.user, botUser, config, envs)
+    val pass = resolve(env.pass, key.pass, botPass, config, envs)
+    val team = resolve(env.team, key.team, botTeam, config, envs)
+    val channel = resolve(env.channel, key.channel, botChannel, config, envs)
+
+    ConfigFactory.parseMap(Map(
+      key.host -> host,
+      key.port -> port,
+      key.user -> user,
+      key.pass -> pass,
+      key.team -> team,
+      key.channel -> channel
+    )).withFallback(config)
+  }
+
+  def resolve(env: String, key: String, default: String, cfg: Config, envs: Map[String, String]) =
+    envs.get(env).orElse(cfg.getAs[String](key)).getOrElse(default)
+}
---
+++ dockerbot/src/test/scala/ConfigurationSpec.scala
@@ -0,0 +1,35 @@
+import com.typesafe.config.ConfigFactory
+import mm4s.dockerbot.{Configuration, ConfigKeys}
+import net.ceedubs.ficus.Ficus._
+import org.scalatest.{Matchers, WordSpec}
+import scala.collection.JavaConversions._
+
+
+/**
+ *
+ */
+class ConfigurationSpec extends WordSpec with Matchers {
+  import ConfigKeys.key._
+
+  "building" should {
+    val expectedhost = "host"
+    val expectedhost2 = "host2"
+
+    implicit val envs = Map[String, String]("MM_HOST" -> expectedhost)
+
+    "give highest priority to env var" in {
+      val config = ConfigFactory.parseMap(Map(host -> expectedhost2))
+      Configuration.build(config).as[String](host) shouldBe expectedhost
+    }
+
+    "prefer configuration over default" in {
+      val config = ConfigFactory.parseMap(Map(host -> expectedhost2))
+      Configuration.build(config)(Map()).as[String](host) shouldBe expectedhost2
+    }
+
+    "use defaults" in {
+      val config = ConfigFactory.parseMap(Map(host -> expectedhost2))
+      Configuration.build(config)(Map()).as[String](pass) shouldBe ConfigKeys.defaults.botPass
+    }
+  }
+}

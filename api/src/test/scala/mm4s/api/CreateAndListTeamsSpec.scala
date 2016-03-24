f41f24aa8a5d3f49faef1f36eb999ea23f2b6d73
John Wass <jwass3@gmail.com>
2016-03-24 19:04:53 -0400

    team creation and listing integration test - first integration test - works from docker-compose in root project

---
+++ api/src/test/scala/mm4s/api/CreateAndListTeamsSpec.scala
@@ -0,0 +1,55 @@
+package mm4s.api
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import akka.testkit.TestKit
+import mm4s.api.Streams._
+import mm4s.api.TeamModels.{CreateTeam, Team, TeamCreated}
+import mm4s.test.IntegrationTest
+import org.scalatest.{Entry, Inside, AsyncWordSpecLike, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+/**
+ * validate the create and list teams api
+ */
+class CreateAndListTeamsSpec extends TestKit(ActorSystem()) with AsyncWordSpecLike with Matchers with IntegrationTest with Inside {
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val name = random()
+  val email = s"a${random()}@bar.com"
+  val cteam = CreateTeam(name, name, email)
+
+  val session = Await.result(token(), 10.seconds)
+
+  s"The $name team" should {
+    import mm4s.api.TeamProtocols._
+
+    "not exist" taggedAs integration in {
+      Teams.list(session.token).via(connection()).mapAsync(1)(r => Unmarshal(r).to[Map[String, Team]]).map { teams =>
+        teams shouldNot matchPattern {
+          case Entry(_, Team(_, name)) =>
+        }
+      }.runWith(Sink.head)
+    }
+
+    "be created" taggedAs integration in {
+      Teams.create(cteam)
+      .via(connection())
+      .via(response[TeamCreated])
+      .runWith(Sink.head).map { res =>
+        res.name shouldBe name
+      }
+    }
+
+    "exist" taggedAs integration in {
+      Teams.list(session.token).via(connection()).mapAsync(1)(r => Unmarshal(r).to[Map[String, Team]])
+      .map(_.values.map(_.name)).map(_ should contain(name))
+      .runWith(Sink.head)
+    }
+  }
+}

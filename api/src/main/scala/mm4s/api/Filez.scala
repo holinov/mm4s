c2d1877cadd5f6de454cdef90d073cb1fea776cb
John Wass <jwass3@gmail.com>
2016-03-26 06:56:51 -0400

    additional file access support - upload - download - roundtrip test

--- api/src/main/scala/mm4s/api/Filez.scala
+++ api/src/main/scala/mm4s/api/Filez.scala
@@ -1,10 +1,13 @@
 package mm4s.api
 
+import java.io.File
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Cookie
+import akka.stream.scaladsl.{FileIO, Source}
 import mm4s.api.Streams._
 import spray.json._
 
@@ -21,16 +24,38 @@ object Filez {
     }
   }
 
+
+  def put(fu: FileUpload, token: String)(implicit system: ActorSystem) = {
+    val file = fu.file
+    val formData = Multipart.FormData(
+      Source(List(
+        Multipart.FormData.BodyPart(
+          "files",
+          HttpEntity(ContentTypes.`text/plain(UTF-8)`, file.length(), FileIO.fromFile(file, chunkSize = 100000)),
+          Map("filename" -> fu.file.getName)
+        ),
+        Multipart.FormData.BodyPart("channel_id", fu.channelId)
+      ))
+    )
+
+    request(s"/files/upload") { r =>
+      Marshal(formData).to[RequestEntity].map(r.withHeaders(Cookie("MMTOKEN", token)).withMethod(HttpMethods.POST).withEntity)
+    }
+  }
+
   def info(path: FilePath, token: String)(implicit system: ActorSystem) = {
     request(s"/files/get_info/${fileurl(path)}") { r =>
       Marshal(path).to[MessageEntity].map(r.withHeaders(Cookie("MMTOKEN", token)).withEntity)
     }
   }
 
-  def fileurl(path: FilePath): String = s"${path.channel_id}/${path.user_id}${path.filename}"
+  def fileurl(path: FilePath): String = s"${path.channel_id}/${path.user_id}/${path.filename}"
 }
 
 object FileModels {
+  case class FileUpload(channelId: String, file: File)
+  case class FilesUploaded(filenames: Seq[String])
+
   case class FilePath(channel_id: String, user_id: String, filename: String)
   case class FileInfo(filename: String, size: Int, extension: String, mime_type: String, has_preview_image: Boolean)
 }
@@ -38,6 +63,7 @@ object FileModels {
 object FileProtocols extends DefaultJsonProtocol with SprayJsonSupport {
   import FileModels._
 
+  implicit val FilesUploadedFormat: RootJsonFormat[FilesUploaded] = jsonFormat1(FilesUploaded)
   implicit val FileInfoFormat: RootJsonFormat[FileInfo] = jsonFormat5(FileInfo)
   implicit val FilePathFormat: RootJsonFormat[FilePath] = jsonFormat3(FilePath)
 }
---
+++ api/src/test/scala/mm4s/api/FileAccessSpec.scala
@@ -0,0 +1,71 @@
+package mm4s.api
+
+import java.nio.file.{Files, Paths}
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.testkit.TestKit
+import mm4s.api.ChannelModels.ChannelListing
+import mm4s.api.FileModels.{FilePath, FileUpload, FilesUploaded}
+import mm4s.api.FileProtocols._
+import mm4s.api.Streams._
+import mm4s.test.IntegrationTest
+import org.scalactic.FutureSugar
+import org.scalatest.{AsyncWordSpecLike, Inside, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+
+class FileAccessSpec extends TestKit(ActorSystem()) with AsyncWordSpecLike with FutureSugar with Matchers with Inside with IntegrationTest {
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val session = Await.result(token(), 10.seconds)
+
+  val fspath = tmp()
+  println(s"temp file at $fspath")
+  val content = s"some-random-content[${random()}]"
+
+  Source.single(content).to(Sink.foreach(c => Files.write(fspath, c.getBytes))).run()
+
+  val userid = session.details.id
+  val channelid = "6wpnfat4tjrntbd8563x9q9bee" // Await.result(channel(), 10.seconds)
+
+
+  "Files" should {
+    "upload" taggedAs integration in {
+      Filez.put(FileUpload(channelid, fspath.toFile), session.token).via(connection()).via(response[FilesUploaded]).map { r =>
+        r.filenames.map(Paths.get(_).getFileName) should contain(fspath.getFileName)
+      }.runWith(Sink.head)
+    }
+
+    "download" taggedAs integration in {
+      val f1 = Filez.put(FileUpload(channelid, fspath.toFile), session.token).via(connection()).via(response[FilesUploaded]).map { r =>
+        r.filenames.head.split("/")
+      }.runWith(Sink.head)
+
+      // split result into channel/user/pre/filename
+      val up = Await.result(f1, 5.seconds).filter(_.nonEmpty)
+      up.length shouldBe 4
+
+      Filez.get(FilePath(up(0), up(1), s"${up(2)}/${up(3)}"), session.token)
+      .via(connection())
+      .mapAsync(1)(Unmarshal(_).to[String]).runWith(Sink.head)
+      .map(_ shouldBe content)
+    }
+  }
+
+  def tmp() = Paths.get(sys.props.getOrElse("java.io.tmpdir", "/tmp"), random())
+  def channel() = {
+    import ChannelProtocols._
+
+    Channels.list(session.token)
+    .via(connection())
+    .via(response[ChannelListing])
+    .map(l => l.channels.seq.head.id)
+    .runWith(Sink.head)
+  }
+}

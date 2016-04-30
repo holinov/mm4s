a709d96ab18cdb0efd395359fc0b11dc8a908994
John Wass <jwass3@gmail.com>
2016-04-30 14:16:46 -0400

    initial multichannel support #32 initial authenticated connection #33

--- bots/src/main/scala/mm4s/bots/Boot.scala
+++ bots/src/main/scala/mm4s/bots/Boot.scala
@@ -8,12 +8,12 @@ import akka.stream.scaladsl.Sink
 import com.rxthings.di._
 import com.typesafe.scalalogging.LazyLogging
 import mm4s.api.Streams._
-import mm4s.api.UserModels.{LoggedInToChannel, LoginByUsername}
+import mm4s.api.UserModels.{LoggedIn, LoggedInToChannel, LoginByUsername}
 import mm4s.api.{Channels, Connection, Users}
 import mm4s.bots.api.{Bot, ConfigKeys, Configuration, Register}
 import net.ceedubs.ficus.Ficus._
 
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration.Duration
 
 /**
@@ -34,26 +34,28 @@ object Boot extends App with LazyLogging {
     user <- config.getAs[String](key.user)
     pass <- config.getAs[String](key.pass)
     team <- config.getAs[String](key.team)
-    channel <- config.getAs[String](key.channel)
-  } yield (host, port, user, pass, team, channel) match {
-    case (_, _, _, _, _, _) =>
-      println(s"host:$host port:$port user:$user pass:$pass team:$team chan:$channel")
+  } yield (host, port, user, pass, team) match {
+    case (_, _, _, _, _) =>
+      logger.info("host:{} port:{} user:{} pass:{} team:{}", host, port.toString, user, pass, team)
 
       val bot: ActorRef = injectActor[Bot]
       val conn: Connection = connection(host, port)
+      val channel = config.getAs[String](key.channel)
 
       Users.login(LoginByUsername(user, pass, team))
       .via(conn)
       .via(Users.extractSession())
       .mapAsync(1) { s =>
-        Channels.list(s.token)
-        .via(conn).via(Channels.findany(channel))
-        .map(c => c.map(c => LoggedInToChannel(s.token, c.id, s.details)).getOrElse(s))
-        .runWith(Sink.head)
+        channel.map { ch =>
+          Channels.list(s.token)
+          .via(conn).via(Channels.findany(ch))
+          .map(c => c.map(c => LoggedInToChannel(s.token, c.id, s.details)).getOrElse(s))
+          .runWith(Sink.head)
+        }.getOrElse(Future.successful(LoggedIn(s.token, s.details)))
       }
       .runWith(Sink.actorRef(Gateway(conn), Register(bot)))
     case _ =>
-      logger.error("configuration failed h[{}],p[{}],u[{}],p[{}],t[{}],c[{}],", host, port.toString, user, pass, team, channel)
+      logger.error("configuration failed h[{}],p[{}],u[{}],p[{}],t[{}],c[{}],", host, port.toString, user, pass, team)
       system.terminate()
   }
 
